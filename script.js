8d2e9e29b8f9345a5fbdbfea98cc1b17bf31a331
Aditya7123421 <adityaconnect2007@gmail.com>
2026-03-23 21:36:41 +0530

    Initial commit - Kryptonix UI clone

---
+++ script.js
@@ -0,0 +1,11 @@
+
+
+// const reviewCards = document.querySelectorAll(".customer-review_card");
+// reviewCards.forEach(card =>{
+//     card.addEventListener("mouseenter", ()=>{
+//         card.classList.add("customer-review_card-active");
+//     })
+//     card.addEventListener("mouseleave", ()=>{
+//         card.classList.remove("customer-review_card-active");
+//     })
+// });
